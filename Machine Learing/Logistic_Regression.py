b8ad1e4453fe79efe75e681d67554b12db5966b9
Divyanshu <divyanshu.gangwar@upkarak.com>
2025-07-02 18:35:33 +0530

    Logistic regression done

--- Machine Learing/Logistic_Regression.py
+++ Machine Learing/Logistic_Regression.py
@@ -8,7 +8,7 @@ from sklearn.model_selection import train_test_split
 dataset=pd.read_csv(r"C:\Users\divya\Downloads\logit classification.csv")
 
 X=dataset.iloc[:,[2,3]].values
-y=dataset.iloc[:,[-1]].values
+y=dataset.iloc[:,-1].values
 
 X_train,X_test,y_train, y_test=train_test_split(X,y,test_size=0.25,random_state=0)
 
@@ -37,4 +37,20 @@ ac=accuracy_score(y_test,y_pred)
 
 from sklearn.metrics import classification_report
 cr=classification_report(y_test, y_pred)
-cr
+
+
+### we need to pass future records to predict 
+
+dataset1=pd.read_csv(r"C:\Users\divya\Downloads\final1.csv")
+
+d2=dataset1.copy()
+
+dataset1=dataset1.iloc[:,[3,4]].values
+
+M=sc.fit_transform(dataset1)
+
+y_pred1=pd.DataFrame()
+
+d2['y_pred1']=classifier.predict(M)
+
+d2.to_csv('final1.csv')
